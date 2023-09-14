205e9671979278f3e21feee0e89f0aa293141f67
OmidrezaBagheriyan <OmidrezaBagheriyan@gmail.com>
2023-09-14 17:47:29 +0330

    Add nest recycler view in HomeFragment.kt

--- app/src/main/java/com/omidrezabagheriyan/nestrecyclerviewtutorial/ui/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/omidrezabagheriyan/nestrecyclerviewtutorial/ui/fragments/home/HomeFragment.kt
@@ -6,14 +6,21 @@ import androidx.fragment.app.Fragment
 import com.omidrezabagheriyan.nestrecyclerviewtutorial.R
 import com.omidrezabagheriyan.nestrecyclerviewtutorial.databinding.FragmentHomeBinding
 import com.omidrezabagheriyan.nestrecyclerviewtutorial.ui.utils.viewBinding
+import com.omidrezabagheriyan.nestrecyclerviewtutorial.utils.FakeData
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
     private val binding by viewBinding<FragmentHomeBinding>()
+    private lateinit var adapter: FieldsOfProgrammingAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        adapter = FieldsOfProgrammingAdapter()
+        adapter.submitList(FakeData.fieldsOfProgramming)
+        binding.rvFieldsOfProgramming.setHasFixedSize(true)
+        binding.rvFieldsOfProgramming.adapter = adapter
     }
 }
