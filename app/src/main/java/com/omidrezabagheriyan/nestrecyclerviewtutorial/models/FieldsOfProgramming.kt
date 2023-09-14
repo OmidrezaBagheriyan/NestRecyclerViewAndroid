8cff4161ded6569855a5ca5c05034190b79ac491
OmidrezaBagheriyan <OmidrezaBagheriyan@gmail.com>
2023-09-14 16:52:45 +0330

    Add models FieldsOfProgramming.kt & ProgrammingLanguages.kt in project

---
+++ app/src/main/java/com/omidrezabagheriyan/nestrecyclerviewtutorial/models/FieldsOfProgramming.kt
@@ -0,0 +1,3 @@
+package com.omidrezabagheriyan.nestrecyclerviewtutorial.models
+
+data class FieldsOfProgramming(val id: Int, val name: String, val languageList: List<ProgrammingLanguages>)
---
+++ app/src/main/java/com/omidrezabagheriyan/nestrecyclerviewtutorial/models/ProgrammingLanguages.kt
@@ -0,0 +1,3 @@
+package com.omidrezabagheriyan.nestrecyclerviewtutorial.models
+
+class ProgrammingLanguages(id: Int, name: String, description: String)
